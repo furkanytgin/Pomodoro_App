a690aa859e0a74cc859995a316879032b1123dca
furkanytgin <yetginfurkan2@gmail.com>
2025-04-27 21:22:53 +0300

    project 101

--- Pomodoro_App/ViewController.swift
+++ Pomodoro_App/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pomodoroCountLabel: UILabel!
+    @IBOutlet weak var progressView: UIProgressView!
+    @IBOutlet weak var timerLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func startButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func pauseButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func resetButtonPressed(_ sender: UIButton) {
+    }
+    
 }
 
